1fc9d9ee5a7024c966619d446ef1c5e3dc5e12fb
Abdulsamii Ajala <kgasta@gmail.com>
2019-11-24 08:06:18 +0100

    fix: code fix

--- src/app.js
+++ src/app.js
@@ -7,6 +7,5 @@ app.set('views', path.join(__dirname, './views'))
   .set('view engine', 'ejs')
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.render('index', { title: 'Index' }))
-  .listen(3000, () => {
-    console.log('PS Project Running on port 3000!')
-  })
+  
+app.listen(3000, () => console.log('PS Project Running on port 3000!'))
